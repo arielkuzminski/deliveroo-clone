16592f288b3036c5742b285a92d1470c7369726a
Ariel <ariel.kuzminski@gmail.com>
2019-04-09 23:12:02 +0200

    Add dummy header - bootstrap works

---
+++ client/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <nuxt-link tag="a" class="navbar-brand" to="/" exact>Deliveroo clone</nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
